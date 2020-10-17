9fc4927e6bee8fad51c2cb21f8ce55819ea69f17
IqbalSaimbhi <saimbhi.iqbal@gmail.com>
2020-10-17 09:18:40 -0400

    Implelented image crousel Home screen

--- pages/home.js
+++ pages/home.js
@@ -2,12 +2,51 @@ import React from "react"
 import Layout from "../layouts"
 import Link from "next/link";
 //import Tabs from "../components/Tabs"
+import Carousel from 'react-bootstrap/Carousel'
+
 
 const home = props => {
   return (
     <Layout>
        <div>
-      <img src={require('../image/picture1.jpg')} class="img-thumbnail"/>
+      {/* <img src={require('../image/picture1.jpg')} class="img-thumbnail"/> */}
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+     src={require('../image/slider1.png')} class="img-thumbnail"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={require('../image/slider1.png')} class="img-thumbnail"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={require('../image/slider1.png')} class="img-thumbnail"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
       </div>
       </Layout>
  
