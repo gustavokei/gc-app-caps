baa41e1027e932379840901c5775ce498a50e001
IqbalSaimbhi <saimbhi.iqbal@gmail.com>
2020-10-26 18:11:22 -0400

    fixing href links

--- components/menu.js
+++ components/menu.js
@@ -15,21 +15,9 @@ const Menu = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
-            <NavDropdown title="Characters" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Character 1
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">
-                Character 2
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">
-                Character 3
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/characters">Characters</Nav.Link>
             <NavDropdown title="Ranking" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">
                 Highest EXP
