f1c7f7b7ab59fe6911f34edfbf460f5852fe6bb5
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2020-11-25 18:46:58 -0300

    created donation page w/ paypal integration

---
+++ pages/donation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "../components/head";
+import Menu from "../components/menu";
+import Footer from "../components/footer";
+import styles from "../styles.module.css";
+import DonationContent from "../components/pages/donation/content.js";
+
+const Donation = () => {
+  return (
+    <div>
+      <Head title="Project Soluna" />
+      <Menu />
+      <div className={styles.content}>
+        <DonationContent />
+      </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Donation;
